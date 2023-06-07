6908b0cd90c1ae987c4108751e2c0da5aa1f4850
Mark <x@markthemark.com>
2023-06-07 17:28:29 -0400

    improvement(cache): redis key namespacing to avoid future collisions

--- api/src/dbs/database_redis.py
+++ api/src/dbs/database_redis.py
@@ -8,3 +8,11 @@ redis_client = redis.Redis(
     db=0,
     decode_responses=True,
 )
+
+def r_key(list_of_values: list[str or int]) -> str:
+    '''
+    Join together a string of strings/ints so multiple publishers to redis don't collide
+    '''
+    if len(list_of_values) < 2:
+        raise ValueError('Not enough specificity for redis key. Collisions can occur')
+    return ':::'.join(list_of_values)
